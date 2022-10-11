f6265f6ed7a4e8f332294f471a1f3d1723b27347
PramodVejendla <pramod.vejendla@gmail.com>
2022-10-11 18:32:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import ProductList from "./ProductList";
+import Promo from "./Promo";
+
+function App() {
+  return (
+    <div>
+     <Header />
+     <ProductList />
+     <Promo />
+    </div>
+  );
+}
+
+export default App;
